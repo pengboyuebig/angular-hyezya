aec3a31fbfba915d41b326c91767b1cf5290fe5a
pengboyuebig <2430633570@qq.com>
2022-07-29 17:52:03 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -81,6 +81,7 @@
         </a-list>
       </a-list-item>
     </a-list>
+    <br />
     <a-popover title="Title">
       <a-button>Hover</a-button>
       <template #content>
@@ -88,6 +89,7 @@
         <p>Here is the text content</p>
       </template>
     </a-popover>
+    <br />
     <a-space size="large">
       <a-statistic title="New Users" :value="125670" show-group-separator>
         <template #suffix>
@@ -121,10 +123,54 @@
       <template #suffix>%</template>
     </a-statistic>
     <a-button @click="start = true">Start</a-button>
+    <br />
+    <!-- 表格 -->
+    <a-space direction="vertical" size="large" fill>
+      <div>
+        <span>OnlyCurrent: </span>
+        <a-switch v-model="rowSelection.onlyCurrent" />
+      </div>
+      <a-table
+        row-key="name"
+        :columns="columns"
+        :data="data"
+        :row-selection="rowSelection"
+        v-model:selectedKeys="selectedKeys"
+        :pagination="pagination"
+      />
+    </a-space>
+    <br />
+    <a-space>
+      <a-tag closable>Tag</a-tag>
+      <a-tag closable>
+        <!-- 表示插入的字体图标的数据 -->
+        <template #icon>
+          <icon-star />
+        </template>
+        Tag
+      </a-tag>
+    </a-space>
+    <!-- 时间轴 -->
+    <a-timeline>
+      <a-timeline-item label="2017-03-10" dotColor="#00B42A">
+        The first milestone
+      </a-timeline-item>
+      <a-timeline-item label="2018-05-22">The second milestone</a-timeline-item>
+      <a-timeline-item label="2020-06-22" dotColor="#F53F3F">
+        The third milestone
+        <IconExclamationCircleFill
+          :style="{ color: 'F53F3F', fontSize: '12px', marginLeft: '4px' }"
+        />
+      </a-timeline-item>
+      <a-timeline-item label="2020-09-30" dotColor="#C9CDD4">
+        The fourth milestone
+      </a-timeline-item>
+    </a-timeline>
   </div>
 </template>
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+import { IconExclamationCircleFill } from '@arco-design/web-vue/es/icon';
 import {
   IconHeart,
   IconMessage,
@@ -152,15 +198,124 @@ export default {
     };
     const align = ref(false);
     const start = ref(false);
+    // table
+    const selectedKeys = ref(['1', '2']);
+
+    const rowSelection = reactive({
+      type: 'checkbox',
+      showCheckedAll: true,
+      onlyCurrent: false,
+    });
+    const pagination = { pageSize: 5 };
+
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+      },
+      {
+        title: 'Salary',
+        dataIndex: 'salary',
+      },
+      {
+        title: 'Address',
+        dataIndex: 'address',
+      },
+      {
+        title: 'Email',
+        dataIndex: 'email',
+      },
+    ];
+    const data = reactive([
+      {
+        key: '1',
+        name: 'Jane Doe',
+        salary: 23000,
+        address: '32 Park Road, London',
+        email: 'jane.doe@example.com',
+      },
+      {
+        key: '2',
+        name: 'Alisa Ross',
+        salary: 25000,
+        address: '35 Park Road, London',
+        email: 'alisa.ross@example.com',
+      },
+      {
+        key: '3',
+        name: 'Kevin Sandra',
+        salary: 22000,
+        address: '31 Park Road, London',
+        email: 'kevin.sandra@example.com',
+        disabled: true,
+      },
+      {
+        key: '4',
+        name: 'Ed Hellen',
+        salary: 17000,
+        address: '42 Park Road, London',
+        email: 'ed.hellen@example.com',
+      },
+      {
+        key: '5',
+        name: 'William Smith',
+        salary: 27000,
+        address: '62 Park Road, London',
+        email: 'william.smith@example.com',
+      },
+      {
+        key: '6',
+        name: 'Jane Doe 2',
+        salary: 15000,
+        address: '32 Park Road, London',
+        email: 'jane.doe@example.com',
+      },
+      {
+        key: '7',
+        name: 'Alisa Ross 2',
+        salary: 28000,
+        address: '35 Park Road, London',
+        email: 'alisa.ross@example.com',
+      },
+      {
+        key: '8',
+        name: 'Kevin Sandra 2',
+        salary: 26000,
+        address: '31 Park Road, London',
+        email: 'kevin.sandra@example.com',
+      },
+      {
+        key: '9',
+        name: 'Ed Hellen 2',
+        salary: 18000,
+        address: '42 Park Road, London',
+        email: 'ed.hellen@example.com',
+      },
+      {
+        key: '10',
+        name: 'William Smith 2',
+        salary: 12000,
+        address: '62 Park Road, London',
+        email: 'william.smith@example.com',
+      },
+    ]);
+
     return {
-      // align,
+      align,
       start,
       like,
       star,
       onLikeChange,
       onStarChange,
+      /*table 数据*/
+      rowSelection,
+      columns,
+      data,
+      selectedKeys,
+      pagination,
     };
   },
+  components: { IconExclamationCircleFill }, // 引入组件
 };
 </script>
 <style scoped>
